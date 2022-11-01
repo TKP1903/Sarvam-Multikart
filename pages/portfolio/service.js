b68d4baab2ea0cbd79c41236be45f31887fb0db9
aaashishsinga567 <70276198+aaashishsinga567@users.noreply.github.com>
2022-11-01 21:59:02 +0530

    removes the build (.next) folder from git

--- pages/portfolio/service.js
+++ pages/portfolio/service.js
@@ -1,8 +1,8 @@
 import React from "react";
 import CommonLayout from '../../components/shop/common-layout';
 import ServiceLayout from "../../components/common/Service/service1";
-import WatchService from "../../components/common/Service/service2";
-import ToolService from "../../components/common/Service/service3";
+// import WatchService from "../../components/common/Service/service2";
+// import ToolService from "../../components/common/Service/service3";
 
 const Service = () => {
     return (
@@ -10,10 +10,10 @@ const Service = () => {
             <div className="section-t-space">
                 <ServiceLayout sectionClass={"service border-section small-section"}  />
             </div>
-            <WatchService />
+            {/* <WatchService />
             <div className="section-b-space">
                 <ToolService />
-            </div>
+            </div> */}
         </CommonLayout>
     )
 }
