29038b5e15ed61dad8b905069eb424a48de476e5
sanjaidebuger <nsksanjai@gmail.com>
2022-11-04 17:14:34 +0530

    Home page banner image updated

--- pages/layouts/Fashion/Components/Banner.js
+++ pages/layouts/Fashion/Components/Banner.js
@@ -7,28 +7,19 @@ import img from 'next/image';
 
 // image 
 import banner1 from "../../../../Assets/hero-image/website022.jpg";
-import banner2 from "../../../../Assets/hero-image/firefighter-4324587_1280.jpg";
-import banner3 from "../../../../Assets/hero-image/fire-fighters-1006924_1280.jpg";
-
-
+import banner2 from "../../../../Assets/hero-image/BANNER.png";
 
 const Data = [
   {
     img: banner1,
-    title: "welcome to Sarvam",
+    title: "",
     // desc: "Fire Safety ",
     link: "",
   },
   {
     img: banner2,
-    title: "welcome to Sarvam",
-    desc: "Fire Safety",
-    link: "",
-  },
-  {
-    img: banner3,
-    title: "welcome to Sarvam",
-    desc: "Fire Safety",
+    title: "",
+    desc: "",
     link: "",
   }
 ];
--- pages/layouts/Fashion/Components/MasterBanner.js
+++ pages/layouts/Fashion/Components/MasterBanner.js
@@ -11,14 +11,9 @@ const MasterBanner = ({ img, title, desc, link, classes, btn, btnClass }) => {
             <Col>
               <div className="slider-contain">
                 <div>
-                  <Img src={img} zindex={-8888} />
-                  <h4 zindex={1}>{title}</h4>
-                  <h1 zindex={1}>{desc}</h1>
-                  <Link href={link} zindex={1}>
-                    <a className={`btn ${btnClass ? btnClass : "btn-solid"}`}>
-                      {btn ? btn : "Shop Now"}{" "}
-                    </a>
-                  </Link>
+                  <Img src={img} />
+                
+                  
                 </div>
               </div>
             </Col>
