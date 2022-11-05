7c54d18d2d610b9ff089083125d18b06d133a760
sanjaidebuger <nsksanjai@gmail.com>
2022-11-05 07:16:58 +0530

    home page banner update

--- pages/layouts/Fashion/Components/MasterBanner.js
+++ pages/layouts/Fashion/Components/MasterBanner.js
@@ -1,8 +1,7 @@
-import Link from "next/link";
 import Img from 'next/image';
 import { Col, Container, Row } from "reactstrap";
 
-const MasterBanner = ({ img, title, desc, link, classes, btn, btnClass }) => {
+const MasterBanner = ({ img,classes }) => {
   return (
     <div>
       <div className={`home ${img} ${classes ? classes : "text-center"}`}>
@@ -10,11 +9,7 @@ const MasterBanner = ({ img, title, desc, link, classes, btn, btnClass }) => {
           <Row>
             <Col>
               <div className="slider-contain">
-                <div>
                   <Img src={img} />
-                
-                  
-                </div>
               </div>
             </Col>
           </Row>
