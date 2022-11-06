548847e72767b9d8cad1fd4b7b2beda974dd1a73
aaashishsinga567 <70276198+aaashishsinga567@users.noreply.github.com>
2022-11-06 16:58:41 +0530

    added the new top client images

--- components/shop/common-layout.js
+++ components/shop/common-layout.js
@@ -2,16 +2,16 @@ import React from "react";
 import HeaderOne from "../headers/header-one";
 import Breadcrubs from "../common/widgets/breadcrubs";
 import Helmet from "react-helmet";
-import favicon from "../../public/assets/images/favicon/1.png";
 import MasterFooter from "../footers/common/MasterFooter";
+import favicon from "../../public/assets/images/favicon/favicon.ico";
 
 const CommonLayout = ({ children, title, parent, subTitle }) => {
   return (
     <>
-      {/* <Helmet>
+      <Helmet>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/x-icon" href={favicon ? favicon : ""} />
-      </Helmet> */}
+      </Helmet>
       <HeaderOne topClass="top-header" logoName="logo.png" />
       <Breadcrubs title={title} parent={parent} subTitle={subTitle} />
       <>{children}</>
