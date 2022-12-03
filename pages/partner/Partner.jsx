c47a11f8a5671d26c96ac3ce1290c287743be252
sanjaidebuger <nsksanjai@gmail.com>
2022-12-03 11:29:21 +0530

    partner page updated

--- pages/partner/Partner.jsx
+++ pages/partner/Partner.jsx
@@ -5,153 +5,130 @@ import bannerimg from "../../Assets/collection banner/Banner-img-template.png";
 
 // img of logos
 
-export default function Regionalpartner() {
-  const imgData = [
-    "/assets/images/logos/1.png",
-    // "/assets/images/logos/2.png",
-    // "/assets/images/logos/3.png",
-    // "/assets/images/logos/4.png",
-    // "/assets/images/logos/5.png",
-    // "/assets/images/logos/6.png",
-    // "/assets/images/logos/7.png",
-  ];
-  
-  
+export default function Partner() {
 
   return (
       <CommonLayout parent="home" title="BUSSINESS PARTNER">
         <Container>
        
-          {/* <Col md="12">
-              {imgData.map((imgSrc, i) => {
-                return (
-                  <div key={i}> 
-                    <div className="logo-block" >
-                      <a href={null}>
-                        <Media src={imgSrc} alt="" style={{FlexDirection:"row"}}/>
-                      </a>
-                    </div>
+
+          {/* Regional partner  */}
+
+      <div className='container section'> 
+        <h1 style={{fontSize:"20px",fontWeight:"bold",color:"#96b357"}}>Regional Partner</h1>
+            <div class="gallery-image">
+              <div class="img-box">
+                <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/3m.png" style={{height:"100px" ,width:"100px",margin:"15px"}} /> 
+                <div class="caption">
+                    <h1>3M India Ltd</h1>
+                    <p class="opacity-low">Personal safety division</p>
                   </div>
-                );
-              })}
-          </Col> */}
+              </div>
+              <div class="img-box">
+                <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/acme.png" style={{height:"100px" ,width:"100px",margin:"15px"}} /> 
+                <div class="caption">
+                    <h1>Acme Fabrik Plast</h1>
+                    <p class="opacity-low">Safety Shoes & Rain wears</p>
+                  </div>
+              </div>
+              <div class="img-box">
+                <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/karam.png" style={{height:"100px" ,width:"100px",margin:"15px"}} /> 
+                <div class="caption">
+                    <h1>Karam Industries</h1>
+                    <p class="opacity-low">Fall Protection & Other PPE’s</p>
+                  </div>
+              </div>
+              <div class="img-box">
+                <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/superhouse.png" style={{height:"100px" ,width:"100px",margin:"15px"}} /> 
+                <div class="caption">
+                    <h1>Superhouse Limited</h1>
+                    <p class="opacity-low">Safety Shoes</p>
+                  </div>
+              </div>
+              <div class="img-box">
+                <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/dupoint.png" style={{height:"100px" ,width:"100px",margin:"15px"}} /> 
+                <div class="caption">
+                    <h1>E.I. Dupont India Pvt Limited</h1>
+                    <p class="opacity-low">Chemical Protective clothings</p>
+                  </div>
+              </div> 
+              <div class="img-box">
+                <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/honeywell_01.png" style={{height:"100px" ,width:"100px",margin:"15px"}} /> 
+                <div class="caption">
+                    <h1>Honeywell International <br /> India Pvt Ltd</h1>
+                    <p class="opacity-low">Safety Products, Electrical Safety</p>
+                  </div>
+              </div> 
+              <div class="img-box">
+                <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/esqaure.png" style={{height:"100px" ,width:"100px",margin:"15px"}} /> 
+                <div class="caption">
+                    <h1>E-Square Alliance Pvt ltd</h1>
+                    <p class="opacity-low">Lockouts & Tag out, Traffic Safety</p>
+                  </div>
+              </div> 
+              <div class="img-box">
+                <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/arvind.png" style={{height:"100px" ,width:"100px",margin:"15px"}} /> 
+                <div class="caption">
+                    <h1>Arvind Ltd</h1>
+                    <p class="opacity-low">PROBAN – FR Coverall</p>
+                  </div>
+              </div> 
+              <div class="img-box">
+                <img src="http://www.sarvamsafety.com/wp-content/uploads/2015/03/honeywell_02.png" style={{height:"100px" ,width:"100px",margin:"15px"}} /> 
+                <div class="caption">
+                    <h1>Honeywell Analytics</h1>
+                    <p class="opacity-low">Gas Detectors (Portable & Fixed)</p>
+                  </div>
+              </div> 
+            </div>
+      </div>     
+          {/* Regional partner end */}
 
-          {/* new style  */}
-          <div class="gallery-image">
-    <div class="img-box">
-      <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/3m.png" alt="" /> 
-      <div class="caption">
-          <p>3M India Ltd</p>
-          <p class="opacity-low">personal safety division</p>
-        </div>
-    </div>
-    <div class="img-box">
-      <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/dupoint.png" alt="" />
-        <div class="caption">
-          <p>E.I. Dupont India Pvt Limited</p>
-          <p class="opacity-low">Chemical Protective clothings</p>
-        </div>
-    </div>
-    <div class="img-box">
-      <img src="http://www.sarvamsafety.com/wp-content/uploads/2015/03/honeywell_02.png" alt="" />     
-        <div class="caption">
-          <p>Honeywell Analytics</p>
-          <p class="opacity-low">Gas Detectors (Portable & Fixed)</p>
-        </div>
-    </div>
-    <div class="img-box">
-      <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/acme.png" alt="" />
-      <div class="transparent-box">
-        <div class="caption">
-          <p>Acme Fabrik Plast</p>
-          <p class="opacity-low">Safety Shoes & Rain wears</p>
-        </div>
-      </div> 
-    </div>
-    <div class="img-box">
-      <img src="" alt="" />
-      <div class="transparent-box">
-        <div class="caption">
-          <p>Sea</p>
-          <p class="opacity-low">Cityscape</p>
-        </div>
-      </div> 
-    </div>
-    <div class="img-box">
-      <img src="" alt="" />
-      <div class="transparent-box">
-        <div class="caption">
-          <p>Vintage</p>
-          <p class="opacity-low">Cinematic</p>
-        </div>
-      </div> 
-    </div>
-  </div>
-          {/* new style end  */}
 
-        <div class="section">
-          <h1>Regional Partner</h1>
-          <div class="grid">
-            
-            <div class="item item--medium">
-              <div class="item__details">
-                Acme Fabrik Plast
+          {/* International parnter start */}
+          <div className='container section'> 
+        <h1 style={{fontSize:"20px",fontWeight:"bold",color:"#96b357"}}>International Partner</h1>
+            <div class="gallery-image">
+              <div class="img-box">
+                <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/justrite.png" style={{height:"100px" ,width:"100px",margin:"15px"}} /> 
+                <div class="caption">
+                    <h1>Justrite India</h1>
+                    <p class="opacity-low">Safety Can & Cabinets for Flammable liquid storage.</p>
+                  </div>
               </div>
-            </div>
-            <div class="item item--medium">
-              <div class="item__details">
-                Karam Industries
+              <div class="img-box">
+                <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/escatech.png" style={{height:"100px" ,width:"100px",margin:"15px"}} /> 
+                <div class="caption">
+                    <h1>ESCA Tech, Inc.</h1>
+                    <p class="opacity-low">D-Lead® products - Hand and Body Wash</p>
+                  </div>
               </div>
-            </div>
-            <div class="item item--medium">
-              <div class="item__details">
-                Superhouse Limited
+              <div class="img-box">
+                <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/auburn.png" style={{height:"100px" ,width:"100px",margin:"15px"}} /> 
+                <div class="caption">
+                    <h1>Auburn Manufacturing</h1>
+                    <p class="opacity-low">FM Approved welding Blankets and curtains</p>
+                  </div>
               </div>
-            </div>
-            <div class="item item--medium">
-              <div class="item__details">
-                E.I. Dupont India Pvt Limited
+              <div class="img-box">
+                <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/tempshield.png" style={{height:"100px" ,width:"100px",margin:"15px"}} /> 
+                <div class="caption">
+                    <h1>Tempshield</h1>
+                    <p class="opacity-low">Cryo gloves and aprons</p>
+                  </div>
               </div>
+              <div class="img-box">
+                <img src="http://www.sarvamsafety.com/wp-content/uploads/2014/12/newpig.png" style={{height:"100px" ,width:"100px",margin:"15px"}} /> 
+                <div class="caption">
+                    <h1>New Pig</h1>
+                    <p class="opacity-low">Sorbents - Maintenance, Oil, Chemical - Spill kit</p>
+                  </div>
+              </div> 
+              
             </div>
-            <div class="item item--medium">
-              <div class="item__details">
-                Honeywell International India Pvt Ltd
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        <div class="section">
-          <h1>International Partner</h1>
-          <div class="grid">
-            <div class="item2 item--medium">
-              <div class="item__details">
-                Justrite India
-              </div>
-            </div>
-            <div class="item2 item--medium">
-              <div class="item__details">
-              ESCA Tech, Inc.
-              </div>
-            </div>
-            <div class="item2 item--medium">
-              <div class="item__details">
-              Auburn Manufacturing
-              </div>
-            </div>
-            <div class="item2 item--medium">
-              <div class="item__details">
-              Tempshield
-              </div>
-            </div>
-            <div class="item2 item--medium">
-              <div class="item__details">
-              New Pig
-              </div>
-            </div>
-            
-          </div>
-        </div>
+      </div>
+          {/* International parnter End */}
+
         </Container>
       </CommonLayout>
     );
